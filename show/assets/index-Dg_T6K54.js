3fc07560ef19344e766a18c80d76269093727065
Oct072021 <1299424192@qq.com>
2025-09-08 18:47:51 +0800

    update

---
+++ show/assets/index-Dg_T6K54.js
@@ -0,0 +1,1 @@
+import{u as a,a as u,h as c}from"./index-BpDqFBGl.js";const p={setup(){const t=a(),e=u(),{params:s,query:o}=t,{path:r}=s;return e.replace({path:"/"+r,query:o}),c}};export{p as default};
